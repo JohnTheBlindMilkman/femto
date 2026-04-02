0a3620951b6c6e1f91da4b3f6de58baabf53a5d3
JohnTheBlindMilkman <gosciu0987654321@gmail.com>
2026-04-02 17:12:10 +0200

    Removed unnecessary files; started working on implementing power function (with arbitrary base and exponent)

--- main2.cxx
+++ main2.cxx
@@ -16,13 +16,13 @@ struct Y
 
 int main()
 {
-    Variable<X> x;
-    Variable<Y> y;
-    Function f = x + y; // x + y
-    Function g = x * y; // xy
+    femto::Variable<X> x;
+    femto::Variable<Y> y;
+    femto::Function f = x + y; // x + y
+    femto::Function g = x * y; // xy
 
-    Function h = f * g; // x^2y + xy^2
-    Function d2h = D(D(h,DiffWrt(x)),DiffWrt(x)); // double differencial wrt. x, i.e. d^2h/dx^2 = 2y
+    femto::Function h = f * g; // x^2y + xy^2
+    femto::Function d2h = femto::d(femto::d(h,femto::diff_wrt(x)),femto::diff_wrt(x)); // double differencial wrt. x, i.e. d^2h/dx^2 = 2y
 
     std::vector<double> vals{-3,-2,-1,0,1,2,3};
     for (const auto &valx : vals)
